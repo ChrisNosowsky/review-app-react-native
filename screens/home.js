4d8b21bb6a4ba9ca810a893525332bf6752605af
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-21 23:43:55 -0400

    touchable opacity added

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Text, Button} from 'react-native'
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native'
 import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
@@ -12,7 +12,14 @@ export default function Home({ navigation }) {
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home screen</Text>
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
