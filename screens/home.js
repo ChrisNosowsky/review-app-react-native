9357315697eb04ae3c29ebe845db7a12c49e552c
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-21 23:35:12 -0400

    update

--- screens/home.js
+++ screens/home.js
@@ -4,14 +4,9 @@ import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
 
-    const pressHandler = () => {
-        navigation.navigate('ReviewDetails')
-    }
-
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home screen</Text>
-            <Button title="go to review dets" onPress={pressHandler} />
         </View>
     )
 }
--- screens/review-details.js
+++ screens/review-details.js
@@ -4,14 +4,9 @@ import { globalStyles } from '../styles/global'
 
 export default function ReviewDetails({ navigation }) {
 
-    const pressHandler = () => {
-        navigation.navigate("Home");
-    }
-
     return (
         <View style={globalStyles.container}>
             <Text>Review details screen</Text>
-            <Button title="back to home screen" onPress={pressHandler} />
         </View>
     )
 }
