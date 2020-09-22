c98af5cf073c55071048f703dde9341cb8dadd93
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-21 23:37:50 -0400

    empty

--- screens/home.js
+++ screens/home.js
@@ -3,6 +3,10 @@ import {StyleSheet, View, Text, Button} from 'react-native'
 import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+        {}
+    ]);
+
 
     return (
         <View style={globalStyles.container}>
