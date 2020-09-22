56dcd5a1cc87708619b13e510e0f138fa4500adc
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-09-21 23:39:37 -0400

    data

--- screens/home.js
+++ screens/home.js
@@ -4,7 +4,9 @@ import { globalStyles } from '../styles/global'
 
 export default function Home({ navigation }) {
     const [reviews, setReviews] = useState([
-        {}
+        { title: 'Movie Example 1', rating: 5, body: 'Review Example 1', key: '1' },
+        { title: 'Movie Example 2', rating: 9, body: 'Review Example 2', key: '2' },
+        { title: 'Movie Example 3', rating: 2, body: 'Review Example 3', key: '3' }
     ]);
 
 
