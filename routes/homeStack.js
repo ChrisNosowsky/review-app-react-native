8adfbefddae9d5077035a9fdef2785d9737f09fd
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-10-01 20:55:11 -0400

    header component setup

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,7 +1,9 @@
-import { createStackNavigator } from 'react-navigation-stack'
-import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import Home from '../screens/home';
 import ReviewDetails from '../screens/review-details';
+import Header from '../shared/header';
+import React from 'react';
 
 const screens = {
     Home: {
