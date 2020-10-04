04952b1b4ec6d73c790d38d03a3e71ff7a57845f
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-10-04 12:45:51 -0400

    finished styling for image rating

--- screens/review-details.js
+++ screens/review-details.js
@@ -21,5 +21,12 @@ export default function ReviewDetails({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-    
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
 });
